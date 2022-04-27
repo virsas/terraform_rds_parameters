4e382ad955322f8e8b49f6abf5620496d57fd452
Stefan Moravcik <stefan@virsas.com>
2022-04-27 20:40:50 +0200

    fixing dynamic parameters

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ resource "aws_db_parameter_group" "params" {
   name   = var.name
   family = var.family
   
-  dynamic "attribute" {
+  dynamic "parameter" {
     for_each = [for p in var.params.list: {
       name = p.name
       value = p.value
       method = p.method
     }]
 
-    parameter {
-      name = attribute.value.name
-      value = attribute.value.value
-      apply_method = attribute.value.method
+    content {
+      name = parameter.value.name
+      value = parameter.value.value
+      apply_method = parameter.value.method
     }
   }
 }
